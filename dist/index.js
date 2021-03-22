44ddac98552fb303c550b4f10bf60e142ca489d0
junhup <junhup.lim@u.nus.edu>
2021-03-22 14:24:16 +0800

    init express server

--- dist/index.js
+++ dist/index.js
@@ -13,13 +13,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Project_1 = require("./entities/Project");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const project = yield orm.em.find(Project_1.Project, {});
-    console.log(project);
+    const app = express_1.default();
+    app.get('/', (_, res) => {
+        res.send('hello world');
+    });
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
 });
 main().catch((err) => {
     console.log(err);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Project } from "./entities/Project";
 import microConfig from './mikro-orm.config'
+import express from 'express';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up()
-    
-    // const project = orm.em.create(Project, {title: 'my first project'});
-    // await orm.em.persistAndFlush(project);
 
-    const project = await orm.em.find(Project, {});
-    console.log(project)
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send('hello world')
+    })
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 };
 
 main().catch((err) => {
