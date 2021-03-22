524727a7e56ae41b2b832554a38c03a268256f9b
junhup <junhup.lim@u.nus.edu>
2021-03-22 13:45:52 +0800

    init mikroOrm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Project } from "./entities/Project";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Project],
+        dbName: 'react',
+        type: 'postgresql',
+        debug: !__prod__
+    });
+
+    const project = orm.em.create(Project, {title: 'my first project'});
+    await orm.em.persistAndFlush(project);
+};
+
+main().catch((err) => {
+    console.log(err)
+});
