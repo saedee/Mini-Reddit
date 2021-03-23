12ac0ea633b272548c5ff1cbe9f1198808ecf9e8
junhup <junhup.lim@u.nus.edu>
2021-03-23 22:57:03 +0800

    small refactor on frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-
+import React from 'react';
+import { createClient, Provider } from 'urql';
 import theme from '../theme'
+
+const client = createClient({url: "http://localhost:4000/graphql"})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
 export default MyApp
