8aff6862eab65d506aa1b9f90c09abf05ad5033f
junhup <junhup.lim@u.nus.edu>
2021-03-23 22:33:01 +0800

    add redis server and add session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,15 +6,39 @@ import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
 import {buildSchema} from 'type-graphql';
 import { PostResolver  } from "./resolvers/post";
-import cors from 'cors';
 import { UserResolver } from './resolvers/user';
+import cors from 'cors';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up()
 
     const app = express();
-    
+
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: __prod__ //cookie only works in https
+            },
+            secret: 'qwfaklfsdadfasdasdffdfk',
+            resave: false,
+        })
+    )
+
     app.use(
         cors({
             origin: "http://localhost:3000",
