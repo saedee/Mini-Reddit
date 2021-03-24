6cb77f00f82e600a265b2662dc01ca72acef38ef
junhup <junhup.lim@u.nus.edu>
2021-03-25 06:56:07 +0800

    fix typescript error

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -58,6 +58,7 @@ export type Mutation = {
   register: UserResponse;
   login: UserResponse;
   remove: Scalars['Boolean'];
+  logout: Scalars['Boolean'];
 };
 
 
@@ -137,6 +138,14 @@ export type LoginMutation = (
   ) }
 );
 
+export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type LogoutMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'logout'>
+);
+
 export type RegisterMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -190,6 +199,15 @@ export const LoginDocument = gql`
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
+};
+export const LogoutDocument = gql`
+    mutation Logout {
+  logout
+}
+    `;
+
+export function useLogoutMutation() {
+  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument);
 };
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
