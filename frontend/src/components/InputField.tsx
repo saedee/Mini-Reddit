8d977566e56178ed262ed4d6ace79478ae8e9f1f
junhup <junhup.lim@u.nus.edu>
2021-03-24 22:28:00 +0800

    set up register page

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,20 @@
+import { FormControl, FormLabel, Input, FormErrorMessage } from "@chakra-ui/react";
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  type?: string;
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({ label, size: _, ...props }) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {() => (
-        <Form>
-          <div> hello </div>
-        </Form>
-      )}
-    </Formik>
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" label="Username"></InputField>
+            <Box mt={4}>
+              <InputField name="password" type="password" label="Password"></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              color="white"
+              backgroundColor="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
