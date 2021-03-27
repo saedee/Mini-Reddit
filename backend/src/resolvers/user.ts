3185591faefe8c655501c78ece8c536f1ad5200a
junhup <junhup.lim@u.nus.edu>
2021-03-27 22:28:18 +0800

    refactor backend user resolver

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -29,6 +29,111 @@ class FieldError {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Query(() => User)
+  user(@Arg("id", () => Int) id: number, @Ctx() { em }: MyContext): Promise<User | null> {
+    return em.findOne(User, { id });
+  }
+
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // you are not logged  in
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("usernameOrEmail") usernameOrEmail: string,
+    @Arg("password") password: string,
+    @Ctx() { em, req }: MyContext,
+  ): Promise<UserResponse | null> {
+    const user = await em.findOne(
+      User,
+      usernameOrEmail.includes("@") ? { email: usernameOrEmail } : { username: usernameOrEmail },
+    );
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "usernameOrEmail",
+            message: "username does not exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    req.session.userId = user.id;
+
+    return {
+      user,
+    };
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: usernamePasswordInput,
+    @Ctx() { em, req }: MyContext,
+  ): Promise<UserResponse | null> {
+    const errors = validateRegister(options);
+    if (errors) {
+      return { errors };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    let user;
+    try {
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          email: options.email,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
+      console.log(user);
+    } catch (err) {
+      if (err.code == "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already taken",
+            },
+          ],
+        };
+      }
+      console.log("message: ", err.message);
+    }
+
+    // store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id;
+    return { user };
+  }
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("token") token: string,
@@ -94,112 +199,6 @@ export class UserResolver {
       `<a href = "http://localhost:3000/change-password/${token}"> reset password  </a>`,
     );
     return true;
-  }
-
-  @Query(() => [User])
-  users(@Ctx() { em }: MyContext): Promise<User[]> {
-    return em.find(User, {});
-  }
-
-  @Query(() => User)
-  user(@Arg("id", () => Int) id: number, @Ctx() { em }: MyContext): Promise<User | null> {
-    return em.findOne(User, { id });
-  }
-
-  @Query(() => User, { nullable: true })
-  async me(@Ctx() { req, em }: MyContext) {
-    // you are not logged  in
-    if (!req.session.userId) {
-      return null;
-    }
-    const user = await em.findOne(User, { id: req.session.userId });
-    return user;
-  }
-
-  @Mutation(() => UserResponse)
-  async register(
-    @Arg("options") options: usernamePasswordInput,
-    @Ctx() { em, req }: MyContext,
-  ): Promise<UserResponse | null> {
-    const errors = validateRegister(options);
-    if (errors) {
-      return { errors };
-    }
-    const hashedPassword = await argon2.hash(options.password);
-    let user;
-    try {
-      const result = await (em as EntityManager)
-        .createQueryBuilder(User)
-        .getKnexQuery()
-        .insert({
-          username: options.username,
-          email: options.email,
-          password: hashedPassword,
-          created_at: new Date(),
-          updated_at: new Date(),
-        })
-        .returning("*");
-      user = result[0];
-      console.log(user);
-    } catch (err) {
-      if (err.code == "23505") {
-        return {
-          errors: [
-            {
-              field: "username",
-              message: "username already taken",
-            },
-          ],
-        };
-      }
-      console.log("message: ", err.message);
-    }
-
-    // store user id session
-    // this will set a cookie on the user
-    // keep them logged in
-    req.session.userId = user.id;
-    return { user };
-  }
-
-  @Mutation(() => UserResponse)
-  async login(
-    @Arg("usernameOrEmail") usernameOrEmail: string,
-    @Arg("password") password: string,
-    @Ctx() { em, req }: MyContext,
-  ): Promise<UserResponse | null> {
-    const user = await em.findOne(
-      User,
-      usernameOrEmail.includes("@") ? { email: usernameOrEmail } : { username: usernameOrEmail },
-    );
-    if (!user) {
-      return {
-        errors: [
-          {
-            field: "usernameOrEmail",
-            message: "username does not exist",
-          },
-        ],
-      };
-    }
-    const valid = await argon2.verify(user.password, password);
-
-    if (!valid) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "incorrect password",
-          },
-        ],
-      };
-    }
-
-    req.session.userId = user.id;
-
-    return {
-      user,
-    };
   }
 
   @Mutation(() => Boolean)
