e7d227f1b846650c03dd6f794150081f7ac940c8
junhup <junhup.lim@u.nus.edu>
2021-03-31 22:55:14 +0800

    add new object type to handle end of pagination

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -2,9 +2,11 @@ import { Post } from "../entities/Post";
 import {
   Arg,
   Ctx,
+  Field,
   FieldResolver,
   Int,
   Mutation,
+  ObjectType,
   Query,
   Resolver,
   Root,
@@ -15,6 +17,15 @@ import { isAuth } from "../middleware/isAuth";
 import { PostInput } from "../types/PostInput";
 import { getConnection } from "typeorm";
 
+@ObjectType()
+class PaginatedPosts {
+  @Field(() => [Post])
+  posts: Post[];
+
+  @Field()
+  hasMore: boolean;
+}
+
 @Resolver(Post)
 export class PostResolver {
   @FieldResolver(() => String)
@@ -22,22 +33,26 @@ export class PostResolver {
     return root.text.slice(0, 50);
   }
 
-  @Query(() => [Post])
-  posts(
+  @Query(() => PaginatedPosts)
+  async posts(
     @Arg("limit", () => Int) limit: number,
     @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
-  ): Promise<Post[]> {
+  ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
+    const realLimitPlusOne = realLimit + 1;
     const qb = getConnection()
       .getRepository(Post)
       .createQueryBuilder("p")
       .orderBy('"createdAt"', "DESC")
-      .take(realLimit);
+      .take(realLimitPlusOne);
 
     if (cursor) {
       qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
     }
-    return qb.getMany();
+
+    const posts = await qb.getMany();
+
+    return { posts: posts.slice(0, realLimit), hasMore: posts.length === realLimitPlusOne };
   }
 
   @Query(() => Post)
