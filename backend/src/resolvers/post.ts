2d2ea2cdf54fa7dc0e591f784bf504a76f68ee64
junhup <junhup.lim@u.nus.edu>
2021-04-01 11:33:16 +0800

    update post query logic with vote status

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -77,11 +77,12 @@ export class PostResolver {
   async posts(
     @Arg("limit", () => Int) limit: number,
     @Arg("cursor", () => String, { nullable: true }) cursor: string | null,
+    @Ctx() { req }: MyContext,
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    const replacements: any[] = [realLimitPlusOne];
+    const replacements: any[] = [realLimitPlusOne, req.session.userId];
     if (cursor) {
       replacements.push(new Date(parseInt(cursor)));
     }
@@ -94,10 +95,15 @@ export class PostResolver {
       'id', u.id,
       'username', u.username,
       'email', u.email
-      ) creator
+      ) creator,
+    ${
+      req.session.userId
+        ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
+        : 'null as "voteStatus"'
+    }
     from post p
     inner join "user" u on u.id = p."creatorId"
-    ${cursor ? `where p."createdAt" < $2` : ""}
+    ${cursor ? `where p."createdAt" < $3` : ""}
     order by p."createdAt" DESC
     limit $1
     `,
