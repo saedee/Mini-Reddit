c091ee53ac7543bc554edc340ea69f1b2dac35c5
junhup <junhup.lim@u.nus.edu>
2021-04-03 12:52:36 +0800

    init individual post page

---
+++ frontend/src/pages/post/[id].tsx
@@ -0,0 +1,29 @@
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { usePostQuery, usePostsQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+const Post = ({}) => {
+  const router = useRouter();
+  const intId = typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
+  if (fetching) {
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
+  }
+
+  return <Layout>{data?.post?.text}</Layout>;
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
