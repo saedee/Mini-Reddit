d67523442b8d5f8aa683e52c726fd489a54769ed
junhup <junhup.lim@u.nus.edu>
2021-04-03 13:04:42 +0800

    add link back to home page on nav bar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -43,7 +43,12 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     );
   }
   return (
-    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4} ml={"auto"}>
+    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4} ml={"auto"} align="center">
+      <NextLink href="/">
+        <Link>
+          <Heading>Mini Reddit</Heading>
+        </Link>
+      </NextLink>
       <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
