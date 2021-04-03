fa328383f9d346f7140d3ac5911108b9744916e1
junhup <junhup.lim@u.nus.edu>
2021-04-03 13:05:08 +0800

    handle post id that dont exist

--- frontend/src/pages/post/[id].tsx
+++ frontend/src/pages/post/[id].tsx
@@ -1,8 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import React from "react";
 import { Layout } from "../../components/Layout";
-import { usePostQuery, usePostsQuery } from "../../generated/graphql";
+import { usePostQuery } from "../../generated/graphql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 
 const Post = ({}) => {
@@ -23,7 +24,20 @@ const Post = ({}) => {
     );
   }
 
-  return <Layout>{data?.post?.text}</Layout>;
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading mb={4}>{data.post.title}</Heading>
+      {data.post.text}
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
