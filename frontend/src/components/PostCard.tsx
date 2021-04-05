852259ff26d6e9985a5ebf9d4edd140eca2e2a30
junhup <junhup.lim@u.nus.edu>
2021-04-05 17:58:24 +0800

    handle showing delete and edit icon only to post owners

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -2,7 +2,7 @@ import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import { Box, Flex, Heading, IconButton, Link, Stack, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
-import { PostsQuery, useDeletePostMutation } from "../generated/graphql";
+import { PostsQuery, useDeletePostMutation, useMeQuery } from "../generated/graphql";
 import { UpdootSection } from "./UpdootSection";
 
 interface PostCardProps {
@@ -10,6 +10,7 @@ interface PostCardProps {
 }
 
 export const PostCard: React.FC<PostCardProps> = ({ data }) => {
+  const [{ data: meData }] = useMeQuery();
   const [, deletePost] = useDeletePostMutation();
   return (
     <Stack spacing={8}>
@@ -28,16 +29,18 @@ export const PostCard: React.FC<PostCardProps> = ({ data }) => {
                 <Text flex={1} mt={4}>
                   {p.textSnippet}
                 </Text>
-                <Box ml="auto">
-                  <NextLink href="/post/edit/[id]" as={`/post/edit/${p.id}`}>
-                    <IconButton mr={4} icon={<EditIcon />} aria-label="Edit Post" />
-                  </NextLink>
-                  <IconButton
-                    icon={<DeleteIcon />}
-                    aria-label="Delete Post"
-                    onClick={() => deletePost({ id: p.id })}
-                  />
-                </Box>
+                {meData?.me?.id !== p.creator.id ? null : (
+                  <Box ml="auto">
+                    <NextLink href="/post/edit/[id]" as={`/post/edit/${p.id}`}>
+                      <IconButton mr={4} icon={<EditIcon />} aria-label="Edit Post" />
+                    </NextLink>
+                    <IconButton
+                      icon={<DeleteIcon />}
+                      aria-label="Delete Post"
+                      onClick={() => deletePost({ id: p.id })}
+                    />
+                  </Box>
+                )}
               </Flex>
             </Box>
           </Flex>
