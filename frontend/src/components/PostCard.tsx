64297e2cf098a3ed5cfa93e6f3e541b7c19ba31d
junhup <junhup.lim@u.nus.edu>
2021-04-05 18:15:22 +0800

    abstract edit delete buttons on a component

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,8 +1,8 @@
-import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { Box, Flex, Heading, IconButton, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
-import { PostsQuery, useDeletePostMutation, useMeQuery } from "../generated/graphql";
+import { PostsQuery } from "../generated/graphql";
+import { EditDeletePostButtons } from "./EditDeletePostButtons";
 import { UpdootSection } from "./UpdootSection";
 
 interface PostCardProps {
@@ -10,8 +10,6 @@ interface PostCardProps {
 }
 
 export const PostCard: React.FC<PostCardProps> = ({ data }) => {
-  const [{ data: meData }] = useMeQuery();
-  const [, deletePost] = useDeletePostMutation();
   return (
     <Stack spacing={8}>
       {data!.posts.posts.map((p) =>
@@ -29,18 +27,9 @@ export const PostCard: React.FC<PostCardProps> = ({ data }) => {
                 <Text flex={1} mt={4}>
                   {p.textSnippet}
                 </Text>
-                {meData?.me?.id !== p.creator.id ? null : (
-                  <Box ml="auto">
-                    <NextLink href="/post/edit/[id]" as={`/post/edit/${p.id}`}>
-                      <IconButton mr={4} icon={<EditIcon />} aria-label="Edit Post" />
-                    </NextLink>
-                    <IconButton
-                      icon={<DeleteIcon />}
-                      aria-label="Delete Post"
-                      onClick={() => deletePost({ id: p.id })}
-                    />
-                  </Box>
-                )}
+                <Box ml="auto">
+                  <EditDeletePostButtons id={p.id} creatorId={p.creator.id} />
+                </Box>
               </Flex>
             </Box>
           </Flex>
