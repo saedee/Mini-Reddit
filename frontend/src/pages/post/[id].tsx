64fc23f6a4d1506eec3658f08ba3036ccc9952bc
junhup <junhup.lim@u.nus.edu>
2021-04-05 18:15:45 +0800

    update frontend for single post

--- frontend/src/pages/post/[id].tsx
+++ frontend/src/pages/post/[id].tsx
@@ -1,6 +1,7 @@
 import { Box, Heading } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import React from "react";
+import { EditDeletePostButtons } from "../../components/EditDeletePostButtons";
 import { Layout } from "../../components/Layout";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { useGetPostFromUrl } from "../../utils/useGetPostFromUrl";
@@ -27,7 +28,8 @@ const Post = ({}) => {
   return (
     <Layout>
       <Heading mb={4}>{data.post.title}</Heading>
-      {data.post.text}
+      <Box mb={4}>{data.post.text}</Box>
+      <EditDeletePostButtons id={data.post.id} creatorId={data.post.creator.id} />
     </Layout>
   );
 };
