8d712586bb74e8c1c6de9ea6d5d6b8257dacb533
junhup <junhup.lim@u.nus.edu>
2021-04-06 11:00:10 +0800

    add error message when query fails for post

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,12 +8,17 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
   const [variables, setVariables] = useState({ limit: 15, cursor: null as null | string });
-  const [{ data, fetching }] = usePostsQuery({
+  const [{ data, error, fetching }] = usePostsQuery({
     variables,
   });
 
   if (!fetching && !data) {
-    return <div> you got query failed for some reasons </div>;
+    return (
+      <div>
+        <div> you got query failed for some reasons </div>
+        <div> {error?.message} </div>
+      </div>
+    );
   }
 
   return (
