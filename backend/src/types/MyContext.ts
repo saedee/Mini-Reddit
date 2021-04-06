2792f333bb1f966a66a9779e96b702e86dc7c8b1
junhup <junhup.lim@u.nus.edu>
2021-04-06 11:01:04 +0800

    update context to add dataloader

--- backend/src/types/MyContext.ts
+++ backend/src/types/MyContext.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
 import { Session } from "express-session";
+import { createUserLoader } from "../utils/createUserLoader";
+import { createUpdootLoader } from "../utils/createUpdootLoader";
 
 export type MyContext = {
   req: Request & { session?: Session & { userId?: number } };
   res: Response;
   redis: Redis;
+  userLoader: ReturnType<typeof createUserLoader>;
+  updootLoader: ReturnType<typeof createUpdootLoader>;
 };
