8d5cb0784d48c3567611d559ab463fda7a9ddc07
junhup <junhup.lim@u.nus.edu>
2021-04-08 11:19:18 +0800

    add favicon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,29 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import React from "react";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title>MiniReddit</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="description" content="Fullstack React project" />
+        {/* <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        /> */}
+        {/* <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="icons/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.png" /> */}
+        {/* <meta name="theme-color" content="#3B69CE" /> */}
+      </Head>
       <ColorModeProvider
         options={{
           useSystemColorMode: true,
