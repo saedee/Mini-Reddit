64babfe6ae33fe227921fdea87c904505c9df569
junhup <junhup.lim@u.nus.edu>
2021-04-09 22:52:44 +0800

    start swap from urql to apollo client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,13 @@ import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import React from "react";
 import theme from "../theme";
 import Head from "next/head";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL as string,
+  credentials: "include",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -19,15 +26,17 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="theme-color" content="#3B69CE" />
       </Head>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
+      <ApolloProvider client={client}>
+        <ChakraProvider resetCSS theme={theme}>
+          <ColorModeProvider
+            options={{
+              useSystemColorMode: true,
+            }}
+          >
+            <Component {...pageProps} />
+          </ColorModeProvider>
+        </ChakraProvider>
+      </ApolloProvider>
     </>
   );
 }
