28f74407c166b2aeeb5d7294569735f969c4d9be
junhup <junhup.lim@u.nus.edu>
2021-04-10 10:23:55 +0800

    update forgot password to apollo

--- frontend/src/pages/forgot-password.tsx
+++ frontend/src/pages/forgot-password.tsx
@@ -9,13 +9,13 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 
 const ForgotPassword: React.FC<{}> = ({}) => {
   const [complete, setComplete] = useState(false);
-  const [, forgotPassword] = useForgotPasswordMutation();
+  const [forgotPassword] = useForgotPasswordMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ email: "" }}
         onSubmit={async (values) => {
-          await forgotPassword(values);
+          await forgotPassword({ variables: values });
           setComplete(true);
         }}
       >
@@ -45,4 +45,4 @@ const ForgotPassword: React.FC<{}> = ({}) => {
   );
 };
 
-export default withUrqlClient(createUrqlClient)(ForgotPassword);
+export default ForgotPassword;
