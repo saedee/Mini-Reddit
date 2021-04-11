c24b77ba5d82dca9c0d82322dca0834bec19b9b7
junhup <junhup.lim@u.nus.edu>
2021-04-11 15:43:29 +0800

    update cache with apollo client on change password

--- frontend/src/pages/change-password/[token].tsx
+++ frontend/src/pages/change-password/[token].tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { InputField } from "../../components/InputField";
 import { Wrapper } from "../../components/Wrapper";
-import { useChangePasswordMutation } from "../../generated/graphql";
+import { MeDocument, MeQuery, useChangePasswordMutation } from "../../generated/graphql";
 import { toErrorMap } from "../../utils/toErrorMap";
 import { withApollo } from "../../utils/withApollo";
 
@@ -23,6 +23,15 @@ const ChangePassword: NextPage = () => {
             variables: {
               newPassword: values.newPassword,
               token: typeof router.query.token === "string" ? router.query.token : "",
+            },
+            update: (cache, { data }) => {
+              cache.writeQuery<MeQuery>({
+                query: MeDocument,
+                data: {
+                  __typename: "Query",
+                  me: data?.changePassword.user,
+                },
+              });
             },
           });
           if (response.data?.changePassword.errors) {
